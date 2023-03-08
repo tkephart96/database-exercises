3e66991bb411b55b8103f626c5177a4f0d6af40d
Tyler Kephart <tkephart96@gmail.com>
2023-03-08 16:27:10 -0600

    upadted answers in db_tables_ex

--- db_tables_exercises.sql
+++ db_tables_exercises.sql
@@ -21,13 +21,13 @@ CREATE TABLE `employees` (
 */
 
 -- 11 Which table(s) do you think contain a numeric type column?
--- salaries for sure and maybe all have an employee id number column
+-- salaries for sure and maybe all have an id number column, unless they use char for id
 -- 12 Which table(s) do you think contain a string type column?
 -- probably all tables but salary since it can have just numbers and maybe date
 -- 13 Which table(s) do you think contain a date type column? 
 -- probably all tables but department unless you maybe have the date department was created
 -- 14 What is the relationship between the employees and the departments tables?
--- dept_no and emp_no are used in the dept_emp table
+-- dept_no and emp_no are used in the dept_emp and dept_manager tables
 /* 15 Show the SQL that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
 CREATE TABLE `dept_manager` (
   `emp_no` int NOT NULL,
