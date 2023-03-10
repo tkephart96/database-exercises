99a6e5ab69d46e0e216ae89192d13aa413e274d9
Tyler Kephart <tkephart96@gmail.com>
2023-03-10 15:17:03 -0600

    added group_by_ex, updt functions_ex answers

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -26,7 +26,7 @@ select count(concat(first_name,' ',last_name))
 ;
 -- 5 
 select concat(first_name,' ',last_name) as full_name
-	,curdate() - hire_date as days_hired 
+    ,datediff(curdate(), hire_date) as days_hired
 	,hire_date,birth_date
 	from employees 
     where hire_date 
@@ -37,7 +37,7 @@ select concat(first_name,' ',last_name) as full_name
 -- 6 
 select min(salary),max(salary) 
 	from salaries
-	where to_date >= curdate()
+	where to_date > now()
 ;
 -- 7
 select lower(
@@ -45,8 +45,9 @@ select lower(
 	substr(first_name,1,1)
 	,substr(last_name,1,4)
     ,'_'
-    ,substr(birth_date,6,2)
-    ,substr(birth_date,3,2)
+    -- ,substr(birth_date,6,2)
+    -- ,substr(birth_date,3,2)
+    ,date_format(birth_date, '%m%y')
 	))
     as username
     ,first_name,last_name,birth_date
---
+++ group_by_exercises.sql
@@ -0,0 +1,144 @@
+use employees;
+select database();
+show tables;
+show create table employees;
+select * from titles;
+
+-- 2 unique titles 7
+select distinct title from titles;
+-- 3
+select last_name
+	from employees 
+	where last_name 
+    like 'e%e'
+    group by last_name
+;
+-- 4
+select first_name,last_name
+	from employees 
+	where last_name 
+    like 'e%e'
+    group by first_name,last_name
+;
+-- 5 Chelq, Lindqvist, Qiwen
+select last_name
+	-- select distinct last_name 
+	from employees 
+    where last_name 
+    like '%q%' 
+    and last_name 
+    not like '%qu%'
+    group by last_name
+;
+-- 6
+select last_name, count(*)
+	from employees 
+    where last_name 
+    like '%q%' 
+    and last_name 
+    not like '%qu%'
+    group by last_name
+;
+-- 7
+select first_name,gender,count(*)
+	from employees 
+    where first_name 
+    in ('Irena', 'Vidya', 'Maya')
+    group by first_name, gender
+;
+-- 8
+select lower(
+	concat(
+	substr(first_name,1,1)
+	,substr(last_name,1,4)
+    ,'_'
+    ,date_format(birth_date, '%m%y')
+	)) as username
+    ,count(*)
+	from employees
+    group by username
+;
+-- 9 there are dupes, most dupes 6x, 13251 unique dupes, 27403 total dupes
+select lower(concat( -- show dupes
+		left(first_name,1),left(last_name,4)
+		,date_format(birth_date,'_%m%y')
+		)) as username
+	,count(*) as duplicate
+	from employees
+    group by username
+    order by count(*) desc
+;
+select count(duplicate) -- count unique dupes
+	from (select lower(concat(
+		left(first_name,1),left(last_name,4)
+		,date_format(birth_date,'_%m%y')
+		)) as username,count(*) as duplicate
+		from employees
+		group by username
+		having duplicate > 1) 
+	as uniq_dupes
+;
+select sum(duplicate) -- count total dupes
+	from (select lower(concat(
+		left(first_name,1),left(last_name,4)
+		,date_format(birth_date,'_%m%y')
+		)) as username,count(*) as duplicate
+		from employees
+		group by username
+		having duplicate > 1) 
+	as tot_dupes
+;
+
+-- BONUS
+show tables;
+select * from salaries;
+-- 
+select emp_no, avg(salary)
+	from salaries
+    where to_date < now()
+    group by emp_no
+;
+-- 
+select * from dept_emp;
+select dept_no, count(*) as emp_count
+	from dept_emp
+    where to_date > now()
+    group by dept_no
+;
+-- 
+select emp_no, count(salary)
+	from salaries
+    group by emp_no
+;
+-- 
+select emp_no, max(salary)
+	from salaries
+    group by emp_no
+;
+--
+select emp_no, min(salary)
+	from salaries
+    group by emp_no
+;
+-- 
+select emp_no, std(salary) as std_dev_sal
+	from salaries
+    -- where to_date < now()
+    group by emp_no
+;
+-- 
+select emp_no, max(salary) as max_salary
+	from salaries
+    group by emp_no
+    having max_salary > 150000
+;
+-- 
+select emp_no, avg(salary) as avg_salary
+	from salaries
+    group by emp_no
+    having avg_salary between 80000 and 90000
+;
+
+
+
+
