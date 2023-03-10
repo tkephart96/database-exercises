fdd0ad44448d3c9e941eeb690ef2f34bf35425a5
Tyler Kephart <tkephart96@gmail.com>
2023-03-10 11:14:32 -0600

    added function_ex

---
+++ functions_exercises.sql
@@ -0,0 +1,55 @@
+use employees;
+select database();
+show tables;
+show create table employees;
+select * from employees;
+-- 2 
+select concat(first_name,' ',last_name) 
+	as full_name 
+	from employees 
+	where last_name 
+    like 'e%e'
+;
+-- 3 
+select upper(concat(first_name,' ',last_name)) 
+	as full_name 
+	from employees 
+	where last_name 
+    like 'e%e'
+;
+-- 4 
+select count(concat(first_name,' ',last_name)) 
+	as full_name 
+	from employees 
+	where last_name 
+    like 'e%e'
+;
+-- 5 
+select concat(first_name,' ',last_name) as full_name
+	,curdate() - hire_date as days_hired 
+	,hire_date,birth_date
+	from employees 
+    where hire_date 
+    like '199%' 
+    and birth_date 
+    like '%12-25' 
+;
+-- 6 
+select min(salary),max(salary) 
+	from salaries
+;
+-- 7
+select lower(
+	concat(
+	substr(first_name,1,1)
+	,substr(last_name,1,4)
+    ,'_'
+    ,substr(birth_date,6,2)
+    ,substr(birth_date,3,2)
+	))
+    as username
+    ,first_name,last_name,birth_date
+	from employees
+;
+
+
